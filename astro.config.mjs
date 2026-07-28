1faf6965330922de278e3b6eb2bd0dece2a7ec36
Rise! <132993532+anonymous99-Rise@users.noreply.github.com>
2026-07-28 19:17:35 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { remarkLangBlocks } from './remark-lang-blocks.mjs';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  site: 'https://adminlove520.github.io',
+  site: 'https://anonymous99-Rise.github.io',
   base: isProduction ? '/xiaoxi-blog' : '/',
   output: 'static',
   markdown: {
